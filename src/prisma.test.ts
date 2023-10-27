19208d357529b889b641da02c85a3d7564fee4d4
kingwang09 <kingwang09@gmail.com>
2023-10-27 15:10:55 +0900

    add prisma jest test

---
+++ src/prisma.test.ts
@@ -0,0 +1,17 @@
+import { PrismaClient, Prisma, User } from '@prisma/client'
+import prisma from './client'
+
+
+it('should create 1 order with an existing customer', async () => {
+    let user: Prisma.UserCreateInput = {
+        email: 'hyungeun.jin@naver.com'
+    };
+
+    let createdUser = await prisma.user.create({
+        data: user
+    });
+
+    console.log('✨ 1 user successfully created!')
+
+    expect(createdUser).toHaveProperty('id', 100)
+});
